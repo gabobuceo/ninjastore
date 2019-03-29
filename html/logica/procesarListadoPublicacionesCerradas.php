66f10eff18f6153744d8053a9f50fd2c32cf2c57
gabobuceo <gabriel@gfernandez.uy>
2019-03-29 18:43:36 -0300

    Arreglos varios - FuncionComprar para mas adelante Se editan mypublication

--- html/logica/procesarListadoPublicacionesCerradas.php
+++ html/logica/procesarListadoPublicacionesCerradas.php
@@ -3,12 +3,10 @@ require_once('../logica/funciones.php');
 require_once('../clases/Publicacion.class.php');
 $config = include('../config/config.php');
 // -------- GET DATA ----
-$idpublicacion = $_SESSION['PubID'];
-
 try {          
   $conex = conectar();			
-  $pu= new Publicacion($idpublicacion);
-  $datos_pu=$pu->consultaUno($conex);
+  $pu= new Publicacion('',$_SESSION['id']);
+  $datos_pu=$pu->consultaTodosUsuarioCerradas($conex);
   if (!empty($datos_pu)){
     return $datos_pu;
   }else{
